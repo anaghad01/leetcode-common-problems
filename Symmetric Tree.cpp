359e1957b2c3d6e6c43d95ea0cf24eb2330f3346
Anagha Dangle <anagha.dangle@gmail.com>
2023-03-13 10:47:59 -0400

    Create Symmetric Tree.cpp

---
+++ Symmetric Tree.cpp
@@ -0,0 +1,41 @@
+
+//Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* t1 = q.front();
+            q.pop();
+            TreeNode* t2 = q.front();
+            q.pop();
+            if (t1 == NULL && t2 == NULL) continue;
+            if (t1 == NULL || t2 == NULL) return false;
+            if (t1->val != t2->val) return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+        
+    }
+};
